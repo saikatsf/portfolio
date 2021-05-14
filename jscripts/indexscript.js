99dac131f2bef173fcb6845ef1f16014bf840ef7
Saikat Fouzdar <saikatmax.sf@gmail.com>
2021-05-14 14:36:06 +0530

    AboutME edit

--- jscripts/indexscript.js
+++ jscripts/indexscript.js
@@ -15,4 +15,19 @@ function detailFunc() {
         detailData = 1;
     }
 }
+function moreFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("moreBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+}
 window.onload = function(){mydetail()};
